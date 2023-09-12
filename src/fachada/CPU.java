2977652a1a5e9d5e29fe5cbd158567c0edbdf458
trejos19 <andrestrejos912@gmail.com>
2023-09-12 11:41:03 -0500

    FACADE

---
+++ src/fachada/CPU.java
@@ -0,0 +1,18 @@
+package fachada;
+
+public class CPU {
+    public void freeze(){
+        System.out.println("Cpu: Congelado (>_<)");
+
+    }
+
+    public void execute (){
+      System.out.println("Cpu: Ejecutandose (*_*)");
+    }
+
+    public void jump(){
+        System.out.println("Cpu: Saltando (@_@)");
+    }
+
+    
+}
---
+++ src/fachada/Memory.java
@@ -0,0 +1,7 @@
+package fachada;
+
+public class Memory {
+    public void load(){
+        System.out.println("Cargando........ Memoria (+_+)");
+    }
+}
